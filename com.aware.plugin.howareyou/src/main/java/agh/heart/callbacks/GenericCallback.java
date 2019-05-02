201d24ac0ecd3bd7b186b597099091cc7aeeb138
filipbiernat <fbiernat@gmail.com>
2019-05-02 12:49:48 +0200

    Generic callback

---
+++ com.aware.plugin.howareyou/src/main/java/agh/heart/callbacks/GenericCallback.java
@@ -0,0 +1,76 @@
+package agh.heart.callbacks;
+
+import android.database.Cursor;
+import android.net.Uri;
+import android.util.Log;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import heart.Callback;
+import heart.WorkingMemory;
+import heart.alsvfd.SimpleNumeric;
+import heart.exceptions.AttributeNotRegisteredException;
+import heart.exceptions.NotInTheDomainException;
+import heart.xtt.Attribute;
+
+public class GenericCallback extends HeaRTCallback implements Callback {
+
+    private String[] dbColumns;
+    private Uri dbUri;
+    Map<String,Double> dbValues;
+
+    protected GenericCallback(String[] dbColumns, Uri dbUri){
+        this.dbColumns = dbColumns;
+        this.dbUri = dbUri;
+        dbValues = new HashMap<>();
+    }
+
+    //Protect from calling default constructor
+    private GenericCallback(){
+        Log.e("TAG", "private GenericCallback() - this should not be executed!");
+    }
+
+    @Override
+    public void execute(Attribute attribute, WorkingMemory workingMemory) {
+        try {
+            updateFields();
+
+            //Fill working memory with updated fields
+            for (Map.Entry<String, Double> entry : dbValues.entrySet()) {
+                fillWorkingMemory(workingMemory, entry.getKey(), entry.getValue());
+            }
+
+        } catch (AttributeNotRegisteredException | NotInTheDomainException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void fillWorkingMemory(WorkingMemory workingMemory, String attributeName, double value)
+            throws AttributeNotRegisteredException, NotInTheDomainException {
+        final String HOWAREYOU_PREFIX = "howareyou_";
+        workingMemory.setAttributeValue(HOWAREYOU_PREFIX + attributeName, new SimpleNumeric(value));
+    }
+
+    private void updateFields() {
+        if (resolver == null) {
+            return;
+        }
+
+        Cursor values = getValues(dbColumns);
+        if (values.getCount() == 0) {
+            return;
+        }
+        values.moveToFirst();
+
+        //Update map instead of separate fields
+        for (String dbColumn : dbColumns){
+            dbValues.put(dbColumn, values.getDouble(values.getColumnIndex(dbColumn)));
+        }
+    }
+
+    private Cursor getValues(String[] columns) {
+        return resolver.query(dbUri, columns, null, null, "timestamp DESC");
+    }
+
+}
--- com.aware.plugin.howareyou/src/main/java/agh/heart/callbacks/HowAreYou_Color.java
+++ com.aware.plugin.howareyou/src/main/java/agh/heart/callbacks/HowAreYou_Color.java
@@ -1,80 +1,17 @@
 package agh.heart.callbacks;
-
-import android.database.Cursor;
-import android.util.Log;
-
+import android.net.Uri;
 import heart.Callback;
-import heart.WorkingMemory;
-import heart.alsvfd.SimpleNumeric;
-import heart.exceptions.AttributeNotRegisteredException;
-import heart.exceptions.NotInTheDomainException;
-import heart.xtt.Attribute;
 
-public class HowAreYou_Color extends HeaRTCallback implements Callback {
+public class HowAreYou_Color extends GenericCallback implements Callback {
 
-    private double howareyou_color_red = 0;
-    private double howareyou_color_green = 0;
-    private double howareyou_color_blue = 0;
-    private double howareyou_color_dropped = 0;
+    private static final String[] DB_COLUMNS = new String[]{
+            com.aware.plugin.howareyou.Provider.Table_Color_Data.COLOR_RED,
+            com.aware.plugin.howareyou.Provider.Table_Color_Data.COLOR_GREEN,
+            com.aware.plugin.howareyou.Provider.Table_Color_Data.COLOR_BLUE,
+            com.aware.plugin.howareyou.Provider.Table_Color_Data.COLOR_DROPPED};
+    private static final Uri DB_URI = com.aware.plugin.howareyou.Provider.Table_Color_Data.CONTENT_URI;
 
-    private String howareyou_color_red_column;
-    private String howareyou_color_green_column;
-    private String howareyou_color_blue_column;
-    private String howareyou_color_dropped_column;
-
-    @Override
-    public void execute(Attribute attribute, WorkingMemory workingMemory) {
-        try {
-            updateFields();
-
-            fillWorkingMemory(workingMemory, howareyou_color_red_column,     howareyou_color_red);
-            fillWorkingMemory(workingMemory, howareyou_color_green_column,   howareyou_color_green);
-            fillWorkingMemory(workingMemory, howareyou_color_blue_column,    howareyou_color_blue);
-            fillWorkingMemory(workingMemory, howareyou_color_dropped_column, howareyou_color_dropped);
-        } catch (AttributeNotRegisteredException | NotInTheDomainException e) {
-            e.printStackTrace();
-        }
-
-        Log.d("FILIP", "r:" + howareyou_color_red +
-                ", g:" + howareyou_color_green +
-                ", b:" + howareyou_color_blue +
-                ", d:" + howareyou_color_dropped);
+    public HowAreYou_Color(){
+        super(DB_COLUMNS, DB_URI);
     }
-
-    private void fillWorkingMemory(WorkingMemory workingMemory, String attributeName, double value)
-            throws AttributeNotRegisteredException, NotInTheDomainException {
-        final String HOWAREYOU_PREFIX = "howareyou_";
-        workingMemory.setAttributeValue(HOWAREYOU_PREFIX + attributeName, new SimpleNumeric(value));
-    }
-
-    private void updateFields() {
-        if (resolver == null) {
-            return;
-        }
-
-        howareyou_color_red_column      = com.aware.plugin.howareyou.Provider.Table_Color_Data.COLOR_RED;
-        howareyou_color_green_column    = com.aware.plugin.howareyou.Provider.Table_Color_Data.COLOR_GREEN;
-        howareyou_color_blue_column     = com.aware.plugin.howareyou.Provider.Table_Color_Data.COLOR_BLUE;
-        howareyou_color_dropped_column  = com.aware.plugin.howareyou.Provider.Table_Color_Data.COLOR_DROPPED;
-
-        Cursor values = getValues(new String[]{
-                howareyou_color_red_column,
-                howareyou_color_green_column,
-                howareyou_color_blue_column,
-                howareyou_color_dropped_column});
-        if (values.getCount() == 0) {
-            return;
-        }
-        values.moveToFirst();
-
-        howareyou_color_red     = values.getDouble(values.getColumnIndex(howareyou_color_red_column));
-        howareyou_color_green   = values.getDouble(values.getColumnIndex(howareyou_color_green_column));
-        howareyou_color_blue    = values.getDouble(values.getColumnIndex(howareyou_color_blue_column));
-        howareyou_color_dropped = values.getDouble(values.getColumnIndex(howareyou_color_dropped_column));
-    }
-
-    private Cursor getValues(String[] columns) {
-        return resolver.query(com.aware.plugin.howareyou.Provider.Table_Color_Data.CONTENT_URI, columns, null, null, "timestamp DESC");
-    }
-
 }
